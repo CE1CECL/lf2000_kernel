1f4171b187d76e7ad57a6e9d61b577ade4df860c
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2025-10-18 13:36:39 -0400

    Update device.c

--- arch/arm/mach-nxp3200/device.c
+++ arch/arm/mach-nxp3200/device.c
@@ -807,26 +807,6 @@ static void set_zforce_irq()
 		case LF2000_BOARD_RIO_FEP_800_327P666:
 			zforce_i2c_info.irq = gpio_to_irq(TP_INT);
 			break;
-		case LF2000_BOARD_LUCY:
-		case LF2000_BOARD_LUCY_PP:
-		case LF2000_BOARD_LUCY_CIP:
-		case LF2000_BOARD_VALENCIA:
-		case LF2000_BOARD_VALENCIA_EP:
-		case LF2000_BOARD_VALENCIA_EP_8:
-		case LF2000_BOARD_VALENCIA_FEP:
-		case LF2000_BOARD_VALENCIA_FEP_8:
-		case LF2000_BOARD_VALENCIA_EP_800_480:
-		case LF2000_BOARD_VALENCIA_EP_800_480_8:
-		case LF2000_BOARD_VALENCIA_FEP_800_480:
-		case LF2000_BOARD_VALENCIA_FEP_800_480_8:
-		case LF2000_BOARD_VALENCIA_KND_800_480:
-		case LF2000_BOARD_VALENCIA_KND_800_480_8:
-		case LF2000_BOARD_VALENCIA_KND_1024_600:
-		case LF2000_BOARD_VALENCIA_KND_1024_600_8:
-		case LF2000_BOARD_VALENCIA_CIP:
-		case LF2000_BOARD_VTK:
-			zforce_i2c_info.irq = gpio_to_irq(zforce_i2c_info.platform_data);
-			break;
  		default:
 			printk(KERN_ERR "%s: Invalid Board Revision 0x%X\n", __func__, system_rev);
 			break;
